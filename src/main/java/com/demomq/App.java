5cb10a72e79a2af3cf8ae9f378555c10516191ad
wangrenyi <wangrenyi@360.cn>
2024-12-14 20:41:47 +0800

    初始化

---
+++ src/main/java/com/demomq/App.java
@@ -0,0 +1,13 @@
+package com.demomq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
