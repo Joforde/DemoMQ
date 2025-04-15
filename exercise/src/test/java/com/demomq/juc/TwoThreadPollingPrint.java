ec2100551ac2c249a8a56caf9e47d8b48b799837
wangrenyi <wangrenyi@360.cn>
2025-04-15 23:41:04 +0800

    [learn] synchronized

--- exercise/src/test/java/com/demomq/juc/TwoThreadPollingPrint.java
+++ exercise/src/test/java/com/demomq/juc/TwoThreadPollingPrint.java
@@ -23,17 +23,16 @@ public class TwoThreadPollingPrint {
 
         public static void main(String[] args) {
             Condition condition = lock.newCondition();
-            Condition condition2 = lock.newCondition();
-            new Thread(() -> playBall("Tom", 1, 2, condition, condition2)).start();
-            new Thread(() -> playBall("Jack", 2, 1, condition2, condition)).start();
+            new Thread(() -> playBall("Tom", 1, 2, condition)).start();
+            new Thread(() -> playBall("Jack", 2, 1, condition)).start();
         }
 
-        public static void playBall(String name, int curTurn, int nextTurn, Condition current, Condition next) {
+        public static void playBall(String name, int curTurn, int nextTurn, Condition condition) {
             while (!gameOver) {
                 try {
                     lock.lock();
                     while (turn != curTurn) {
-                        current.await();
+                        condition.await();
                     }
                     if (gameOver) {
                         return;
@@ -46,7 +45,7 @@ public class TwoThreadPollingPrint {
                         System.out.println(name + " take the ball " + count++ + " by " + num);
                     }
                     turn = nextTurn;
-                    next.signal();
+                    condition.signal();
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 } finally {
@@ -87,6 +86,59 @@ public class TwoThreadPollingPrint {
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
+            }
+        }
+    }
+
+    static class SynchronizedSolution2 {
+        /**
+         * 1、Monitor机制
+         * 每个Java对象（如代码中的lock对象）都关联一个Monitor，包含同步队列（竞争锁的线程队列）和等待队列（调用wait()的线程队列）。
+         * 线程进入synchronized代码块时，会尝试通过monitorenter指令获取Monitor所有权；退出时通过monitorexit释放锁。
+         * 2、锁升级过程
+         * 偏向锁：无竞争时，锁记录线程ID，减少同步开销。
+         * 轻量级锁：通过CAS自旋竞争锁，适用于低并发场景。
+         * 重量级锁：竞争激烈时，线程进入阻塞状态，依赖操作系统互斥量（Mutex）
+         * 3、synchronized默认非公平锁，可能导致线程饥饿。若需公平性，需改用ReentrantLock
+         */
+        static Object lock = new Object();
+        static int num = -1;
+        static int count = 0;
+        static int turn = 1;
+        static boolean gameOver = false;
+
+        public static void main(String[] args) {
+            new Thread(() -> playBall("Tom", 1, 2)).start();
+            new Thread(() -> playBall("Jack", 2, 1)).start();
+        }
+
+        static void playBall(String name, int curTurn, int nextTurn) {
+            while (!gameOver) {
+                //竞争的线程进入lock的同步队列
+                synchronized (lock) {
+                    //if可能导致虚假唤醒
+                    while (turn != curTurn) {
+                        try {
+                            //wait调用后，释放锁，当前线程进入锁的等待队列
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    if (gameOver) {
+                        return;
+                    }
+                    lock.notify();
+                    turn = nextTurn;
+                    if (num % 13 == 0) {
+                        gameOver = true;
+                        System.out.println(name + " win the game");
+                    } else {
+                        num = new Random().nextInt(100);
+                        System.out.println(name + " take the ball " + count++ + " by " + num);
+                    }
+                }
+
             }
         }
     }
@@ -138,4 +190,3 @@ public class TwoThreadPollingPrint {
 
     }
 }
-
